0a9992b4ade7d601f7e319f4c8ca9094d2d47cdc
Gabriel Soares gomes <dev.gabrielsoares221@gmail.com>
2022-03-31 16:45:46 -0300

    feat: Melhorando templates

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from turtle import title
 from flask import Flask, redirect, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
@@ -10,7 +11,7 @@ db = SQLAlchemy(app)
 #Criar um model do database
 class Estoque(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    produto = db.Column(db.String(200), nullable=False) #200 é o maximo de caracteres
+    nome = db.Column(db.String(200), nullable=False) #200 é o maximo de caracteres
     data_cadastro = db.Column(db.DateTime, default=datetime.utcnow)
 
     #Função para retornar uma string quando adicionar algo ao banco
@@ -20,24 +21,25 @@ class Estoque(db.Model):
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", active="index", title="Home", page_title="Home") 
 
-@app.route("/produtos")
+@app.route("/produtos/")
 def listarProdutos():
-    return render_template('produtos.html')
+    produtos = Estoque.query.order_by(Estoque.data_cadastro)
+    return render_template('produtos.html', lista_produtos=produtos, active="produtos", title="Produtos", page_title="Produtos")
 
 @app.route("/produtos/adicionar", methods=['POST', 'GET'])
 def adicionarProdutos():
 
     if request.method == "POST":
-        produto_name = request.form['produto']
+        produto_name = request.form['nome']
         new_produto = Estoque(produto=produto_name)
         
         #Mandar para o database
         try:
             db.session.add(new_produto)
             db.session.commit()
-            return redirect("/produtos/adicionar")
+            return redirect("/produtos")
         except:
             return "Ocorreu um erro ao adicionar o produto!"
     else:
